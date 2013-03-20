b0d55be76b7ee876d2d1101547b11d5d44b38e87
Benwei Zhu <benw1988@yeah.net>
2013-03-20 21:42:12 +0800

    修改Executor的测试

--- test/java/org/thoughtworks/zeph/rich/executor/ExecutorTest.java
+++ test/java/org/thoughtworks/zeph/rich/executor/ExecutorTest.java
@@ -1,16 +1,17 @@
 package org.thoughtworks.zeph.rich.executor;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.thoughtworks.zeph.rich.map.*;
+import org.thoughtworks.zeph.rich.map.FirstMapFactory;
+import org.thoughtworks.zeph.rich.map.Grid;
+import org.thoughtworks.zeph.rich.map.Land;
+import org.thoughtworks.zeph.rich.map.MapFactory;
 import org.thoughtworks.zeph.rich.player.Player;
 import org.thoughtworks.zeph.rich.player.PlayerFactoryImp;
 import org.thoughtworks.zeph.rich.props.Block;
 import org.thoughtworks.zeph.rich.props.Bomb;
 import org.thoughtworks.zeph.rich.props.Prop;
-import org.thoughtworks.zeph.rich.props.Robot;
 import org.thoughtworks.zeph.rich.syntax.SyntaxParserFactory;
 
 import static org.hamcrest.CoreMatchers.not;
@@ -24,11 +25,12 @@ public class ExecutorTest {
 	private SyntaxParserFactory parserFactory = new SyntaxParserFactory();
 	private MapFactory mapFactory = new FirstMapFactory();
 	private Grid[] map;
+	private Executor executor;
 
 	@Before
 	public void setUp() {
 		map = mapFactory.createMap();
-		player = new PlayerFactoryImp().createPlayer(1, 10000);
+		player.addGamePoint(1000);
 	}
 
 	@After
@@ -37,67 +39,57 @@ public class ExecutorTest {
 	}
 
 	@Test
-	public void should_return_bomb_from_map_5_when_input_is_bomb_5() {
-		player.addGamePoint(1000);
-		player.buyProp(new Bomb());
-		String instruction = "bomb 5";
-		parserFactory.buildSyntaxParser(instruction, map, player).parser().execute();
-		Prop bomb = new Bomb();
-		assertThat(map[5].getProp(), is(bomb));
-		assertThat(player.getProps().size(), is(0));
-	}
-
-	@Test
-	public void should_return_a_block_when_input_is_block_3() {
-		player.addGamePoint(1000);
+	public void grid_5_should_has_a_block_when_parameter_n_is_5() {
 		player.buyProp(new Block());
-		String instruction = "block 3";
-		Prop block = new Block();
-		parserFactory.buildSyntaxParser(instruction, map, player).parser().execute();
-		assertThat(map[3].getProp(), is(block));
-		assertThat(player.getProps().size(), is(0));
+		int n = 5;
+		executor = new BlockExecutor(map, player, n);
+		executor.execute();
+		Prop expectProp = new Block();
+		assertThat(map[5].getProp(), is(expectProp));
+
 	}
 
 	@Test
-	public void should_not_stop_at_0_when_roll() {
-		String instruction = "roll";
-		parserFactory.buildSyntaxParser(instruction, map, player).parser().execute();
+	public void grid_5_should_has_a_bomb_when_parameter_n_5() {
+		player.buyProp(new Bomb());
+		int n = 5;
+		executor = new BombExecutor(map, player, n);
+		executor.execute();
+		Prop expectProp = new Bomb();
+		assertThat(map[5].getProp(), is(expectProp));
+	}
+
+	@Test
+	public void should_not_stand_on_grid_0_when_execute_roll() {
+		executor = new RollExecutor(map, player);
+		executor.execute();
 		assertThat(player.getCurrentMapPosition(), not(0));
 	}
 
 	@Test
 	public void should_return_null_when_robot() {
-		player.addGamePoint(1000);
-		player.buyProp(new Bomb());
 		player.buyProp(new Block());
-		player.buyProp(new Robot());
-		String instructionRobot = "robot";
-		String instructionBomb = "bomb 5";
-		String instructionBlock = "block 3";
-		parserFactory.buildSyntaxParser(instructionBomb, map, player).parser().execute();
-		parserFactory.buildSyntaxParser(instructionBlock, map, player).parser().execute();
-		parserFactory.buildSyntaxParser(instructionRobot, map, player).parser().execute();
-		for (int i = 1; i <= 10; i++) {
-			assertNull(map[i].getProp());
-		}
+		int n = 5;
+		executor = new BlockExecutor(map, player, n);
+		executor.execute();
+		executor = new RobotExecutor(map, player);
+		executor.execute();
+		assertNull(map[5].getProp());
 	}
 
 	@Test
-	public void should_return__10000_null_and_200_when_sell_5() {
+	public void should_return_10000_when_sell_5() {
 		player.buyLand((Land) map[5]);
-		String instruction = "sell 5";
-		parserFactory.buildSyntaxParser(instruction, map, player).parser().execute();
+		executor = new SellExecutor(map, player, 5);
+		executor.execute();
 		assertThat(player.getMoney(), is(10000));
-		assertNull(((Land) map[5]).getBelongTo());
 	}
 
 	@Test
-	public void should_return_50_and_0_when_sell_tool_1() {
-		player.addGamePoint(100);
+	public void should_return_1000_when_sell_tool_1() {
 		player.buyProp(new Block());
-		String instruction = "sellTool 1";
-		parserFactory.buildSyntaxParser(instruction, map, player).parser().execute();
-		assertThat(player.getGamePoint(), CoreMatchers.is(100));
-		assertThat(player.getProps().size(),is(0));
+		executor = new SellToolExecutor(map,player,1);
+		executor.execute();
+		assertThat(player.getGamePoint(), is(1000));
 	}
 }
--- test/java/org/thoughtworks/zeph/rich/syntax/SyntaxTest.java
+++ test/java/org/thoughtworks/zeph/rich/syntax/SyntaxTest.java
@@ -34,48 +34,48 @@ public class SyntaxTest {
 	public void should_return_a_block_syntax_parser_when_input_is_block_5() {
 		instruction = "block 5";
 		expectParser = new BlockSyntaxParser(instruction, map, player);
-		assertThat(expectParser, is(parserFactory.buildSyntaxParser(instruction, map, player)));
+		assertThat(parserFactory.buildSyntaxParser(instruction, map, player),is(expectParser));
 	}
 
 	@Test
 	public void should_return_a_bomb_syntax_parser_when_input_is_bomb_6() {
 		instruction = "bomb 6";
 		expectParser = new BombSyntaxParser(instruction, map, player);
-		assertThat(expectParser, is(parserFactory.buildSyntaxParser(instruction, map, player)));
+		assertThat(parserFactory.buildSyntaxParser(instruction, map, player),is(expectParser));
 	}
 
 	@Test
 	public void should_return_a_robot_syntax_parser_when_input_is_robot() {
 		instruction = "robot";
 		expectParser = new RobotSyntaxParser(instruction, map, player);
-		assertThat(expectParser, is(parserFactory.buildSyntaxParser(instruction, map, player)));
+		assertThat(parserFactory.buildSyntaxParser(instruction, map, player),is(expectParser));
 	}
 
 	@Test
 	public void should_return_a_roll_syntax_parser_when_input_is_roll() {
 		instruction = "roll";
 		expectParser = new RollSyntaxParser(instruction, map, player);
-		assertThat(expectParser, is(parserFactory.buildSyntaxParser(instruction, map, player)));
+		assertThat(parserFactory.buildSyntaxParser(instruction, map, player),is(expectParser));
 	}
 
 	@Test
 	public void should_return_a_sell_syntax_parser_when_input_is_sell_21() {
 		instruction = "sell 21";
 		expectParser = new SellSyntaxParser(instruction, map, player);
-		assertThat(expectParser, is(parserFactory.buildSyntaxParser(instruction, map, player)));
+		assertThat(parserFactory.buildSyntaxParser(instruction, map, player),is(expectParser));
 	}
 
 	@Test
 	public void should_return_a_sell_tool_syntax_parser_when_input_is_sellTool_1() {
 		instruction = "sellTool 1";
 		expectParser = new SellToolSyntaxParser(instruction, map, player);
-		assertThat(expectParser, is(parserFactory.buildSyntaxParser(instruction, map, player)));
+		assertThat(parserFactory.buildSyntaxParser(instruction, map, player),is(expectParser));
 	}
 
 	@Test
 	public void should_return_a_query_syntax_parser_when_input_is_query() {
 		instruction = "query";
 		expectParser = new QuerySyntaxParser(player);
-		assertThat(expectParser, is(parserFactory.buildSyntaxParser(instruction, map, player)));
+		assertThat(parserFactory.buildSyntaxParser(instruction, map, player),is(expectParser));
 	}
 }
