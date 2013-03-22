18b3244b1d3d0f4e2e56e2aa65e3a1853c383c54
Benwei Zhu <benw1988@yeah.net>
2013-03-22 13:45:28 +0800

    添加Hospital测试

--- main/java/org/thoughtworks/zeph/rich/map/unit/Hospital.java
+++ main/java/org/thoughtworks/zeph/rich/map/unit/Hospital.java
@@ -10,6 +10,11 @@ public class Hospital extends Grid {
 
 	private Set<Player> players;
 
+	public Hospital(int mapId, char symbol) {
+		super(mapId, symbol);
+		players = new HashSet<Player>();
+	}
+
 	@Override
 	public void doesWhatItNeedToDo(Player player) {
 		Iterator it = players.iterator();
@@ -18,38 +23,28 @@ public class Hospital extends Grid {
 			patient.setHospitalDays(0);
 		}
 		players.clear();
-		//release all prisoner
-	}
-
-	@Override
-	public int getPrice() {
-		return 0;  //To change body of implemented methods use File | Settings | File Templates.
-	}
-
-	@Override
-	public int getLevel() {
-		return 0;  //To change body of implemented methods use File | Settings | File Templates.
-	}
-
-	@Override
-	public void setOwner(Player player) {
-		//To change body of implemented methods use File | Settings | File Templates.
-	}
-
-	public Hospital(int mapId, char symbol) {
-		super(mapId, symbol);
-		players = new HashSet<Player>();
-	}
-
-	public void addRole(Player player) {
-		players.add(player);
 	}
 
 	public Set<Player> getPlayers() {
 		return players;
 	}
 
-	public void release() {
-		players.clear();
+	public void addPlayer(Player player) {
+		players.add(player);
+	}
+
+	@Override
+	public int getPrice() {
+		return 0;
+	}
+
+	@Override
+	public int getLevel() {
+		return 0;
+	}
+
+	@Override
+	public void setOwner(Player player) {
+
 	}
 }
--- test/java/org/thoughtworks/zeph/rich/map/HospitalTest.java
+++ test/java/org/thoughtworks/zeph/rich/map/HospitalTest.java
@@ -5,9 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.thoughtworks.zeph.rich.map.unit.Hospital;
 import org.thoughtworks.zeph.rich.player.Player;
+import org.thoughtworks.zeph.rich.player.PlayerFactory;
 import org.thoughtworks.zeph.rich.player.PlayerFactoryImp;
-
-import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -15,14 +14,11 @@ import static org.junit.Assert.assertThat;
 public class HospitalTest {
 	private Player player;
 	private Hospital hospital;
-	private Player aTuBo;
+	private PlayerFactory playerFactory = new PlayerFactoryImp();
 
 	@Before
 	public void setUp() {
-
-		player = new PlayerFactoryImp().createPlayer(1, 10000);
-		hospital = new Hospital(1, 'H');
-		aTuBo = new PlayerFactoryImp().createPlayer(2, 10000);
+		player = playerFactory.createPlayer(1, 10000);
 	}
 
 	@After
@@ -31,24 +27,11 @@ public class HospitalTest {
 	}
 
 	@Test
-	public void should_return_1_when_a_role_hit_by_bomb() {
-		hospital.addRole(player);
-		Set<Player> players = hospital.getPlayers();
-		assertThat(players.size(), is(1));
-	}
-
-	@Test
-	public void should_return_2_when_one_round_finish() {
-		player.setHospitalDays(3);
-		player.countDownHospitalDays();
-		assertThat(player.getHospitalDays(), is(2));
-	}
-
-	@Test
-	public void should_return_0_when_prison_release_all_prisoner() {
-		hospital.addRole(player);
-		hospital.addRole(aTuBo);
-		hospital.release();
+	public void should_return_0_when_a_player_stay_in_hospital() {
+		hospital = new Hospital(1, 'H');
+		Player anotherPlayer = playerFactory.createPlayer(2, 10000);
+		hospital.addPlayer(anotherPlayer);
+		hospital.doesWhatItNeedToDo(player);
 		assertThat(hospital.getPlayers().size(), is(0));
 	}
 }
